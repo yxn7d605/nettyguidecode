b90afb2be32b944a007c62d6f3f296c642549e60
yxn7d605 <yxn7d605@126.com>
2020-05-07 18:14:08 +0800

    add

---
+++ src/main/java/com/yx/home/netty/ch2_4/TimeServer.java
@@ -0,0 +1,13 @@
+package com.yx.home.netty.ch2_4;
+
+public class TimeServer {
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            port = Integer.valueOf(args[0]);
+        }
+
+        AsyncTimeServerHandler timeServer = new AsyncTimeServerHandler(port);
+        new Thread(timeServer, "AIO-AsyncTimeServerHandler-001").start();
+    }
+}
