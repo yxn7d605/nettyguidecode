f0f2ba869d78193aa0468e2b320e05502ae0206e
yxn7d605 <yxn7d605@126.com>
2020-05-09 17:43:24 +0800

    add

---
+++ src/main/java/com/yx/home/netty/ch2_4/ConcurrencyTest.java
@@ -0,0 +1,45 @@
+package com.yx.home.netty.ch2_4;
+
+public class ConcurrencyTest {
+    public static long count = 1000000L;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    public static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("concurrency: " + time + "ms, b=" + b);
+    }
+
+    public static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial: " + time + "ms, b=" + b + ", a=" + a);
+    }
+}
